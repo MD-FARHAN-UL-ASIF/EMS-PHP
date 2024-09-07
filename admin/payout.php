85f26fa08bd5c7249a7ec7f9b9802861ef981691
MD-FARHAN-UL-ASIF <farhanchowdhury66@gmail.com>
2024-09-07 22:15:41 +0600

    latest Update

--- admin/payout.php
+++ admin/payout.php
@@ -7,7 +7,7 @@ if (empty($_SESSION['admin_login'])) {
     header('location: login.php');
     exit(); // Add exit to stop further execution
 }
-$page_title = "Employee Payout Hisotry";
+$page_title = "Employee Payout History";
 $breadcrumb = "Employee Payout";
 
 if (isset($_GET['del'])) {
@@ -25,7 +25,7 @@ if (isset($_GET['del'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EMS</title>
+    <title><?php echo htmlentities($page_title); ?></title>
     <link rel="shortcut icon" type="image/png" href="../assets/images/icon/favicon.ico">
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/font-awesome.min.css">
@@ -33,24 +33,19 @@ if (isset($_GET['del'])) {
     <link rel="stylesheet" href="../assets/css/metisMenu.css">
     <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="../assets/css/slicknav.min.css">
-    <!-- amchart css -->
-    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-    <!-- Start datatable css -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.jqueryui.min.css">
-    <!-- others css -->
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
     <link rel="stylesheet" href="../assets/css/typography.css">
     <link rel="stylesheet" href="../assets/css/default-css.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="stylesheet" href="../assets/css/responsive.css">
-    <!-- modernizr css -->
     <script src="../assets/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 
 <body>
-    <!-- preloader area start -->
     <div id="preloader">
         <div class="loader"></div>
     </div>
@@ -75,9 +70,7 @@ if (isset($_GET['del'])) {
             <?php include '../admin/layout/header.php'; ?>
 
             <div class="main-content-inner">
-                <!-- row area start -->
                 <div class="row">
-                    <!-- Dark table start -->
                     <div class="col-12 mt-5">
                         <div class="card">
                             <?php if ($error) { ?>
@@ -101,7 +94,7 @@ if (isset($_GET['del'])) {
                                     <div style="display: flex; justify-content: flex-end;">
                                         <a href="create_employee_salary.php" class="btn btn-sm btn-info">Give Salary</a>
                                     </div>
-                                    <table id="dataTable3" class="table table-hover table-striped text-center">
+                                    <table id="employeePayoutTable" class="table table-hover table-striped text-center">
                                         <thead class="text-capitalize">
                                             <tr>
                                                 <th>#</th>
@@ -110,12 +103,11 @@ if (isset($_GET['del'])) {
                                                 <th>Month</th>
                                                 <th>Balance</th>
                                                 <th>Payout Date</th>
-                                                <th></th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <?php
-                                            $sql = "SELECT es.id, e.FirstName, e.LastName, es.salary_in, es.month, es.balance, es.payout_date FROM employee_salary es JOIN employees e ON es.empId = e.id";
+                                            $sql = "SELECT es.id, e.FirstName, e.LastName, es.salary_in, es.month, es.balance, es.payout_date FROM employee_salary es JOIN employees e ON es.empId = e.id ORDER BY es.id DESC";
                                             $query = $dbh->prepare($sql);
                                             $query->execute();
                                             $results = $query->fetchAll(PDO::FETCH_OBJ);
@@ -125,39 +117,32 @@ if (isset($_GET['del'])) {
                                             ?>
                                                     <tr>
                                                         <td><?php echo htmlentities($cnt); ?></td>
-                                                        <td> <?php echo htmlentities($result->FirstName . " " . $result->LastName); ?>
-                                                        </td>
+                                                        <td><?php echo htmlentities($result->FirstName . " " . $result->LastName); ?></td>
                                                         <td><?php echo htmlentities($result->salary_in); ?></td>
                                                         <td><?php echo htmlentities($result->month); ?></td>
                                                         <td><?php echo htmlentities($result->balance); ?></td>
                                                         <td><?php echo htmlentities($result->payout_date); ?></td>
-                                                        <td>
-                                                            
-                                                        </td>
                                                     </tr>
-                                                <?php $cnt++;
+                                            <?php
+                                                    $cnt++;
                                                 }
-                                            } else { ?>
-                                                <tr>
-                                                    <td colspan="7">No records found</td>
-                                                </tr>
-                                            <?php } ?>
+                                            }
+                                            ?>
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <!-- Dark table end -->
                 </div>
-                <!-- row area end -->
             </div>
-            <?php include '../admin/layout/footer.php'; ?>
         </div>
+
+        <?php include '../admin/layout/footer.php'; ?>
     </div>
-    <!-- jquery latest version -->
+
+    <!-- jQuery and Bootstrap Scripts -->
     <script src="../assets/js/vendor/jquery-2.2.4.min.js"></script>
-    <!-- bootstrap 4 js -->
     <script src="../assets/js/popper.min.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
     <script src="../assets/js/owl.carousel.min.js"></script>
@@ -165,31 +150,67 @@ if (isset($_GET['del'])) {
     <script src="../assets/js/jquery.slimscroll.min.js"></script>
     <script src="../assets/js/jquery.slicknav.min.js"></script>
 
-    <!-- start chart js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <!-- start highcharts js -->
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <!-- start zingchart js -->
-    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
-    <script>
-        zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
-        ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
-    </script>
-    <!-- all line chart activation -->
-    <script src="../assets/js/line-chart.js"></script>
-    <!-- all pie chart -->
-    <script src="../assets/js/pie-chart.js"></script>
-
-    <!-- Start datatable js -->
+    <!-- DataTables JS and Buttons Extension -->
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
-    <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
 
-    <!-- others plugins -->
+    <!-- Custom Scripts -->
     <script src="../assets/js/plugins.js"></script>
     <script src="../assets/js/scripts.js"></script>
+
+    <!-- DataTable Initialization -->
+    <script>
+        $(document).ready(function() {
+            $('#employeePayoutTable').DataTable({
+                dom: 'Bfrtip', // Add buttons UI
+                buttons: [
+                    {
+                        extend: 'copyHtml5',
+                        text: 'Copy',
+                        exportOptions: {
+                            columns: ':not(:last-child)' // Exclude action column
+                        }
+                    },
+                    {
+                        extend: 'excelHtml5',
+                        text: 'Excel',
+                        exportOptions: {
+                            columns: ':not(:last-child)'
+                        }
+                    },
+                    {
+                        extend: 'csvHtml5',
+                        text: 'CSV',
+                        exportOptions: {
+                            columns: ':not(:last-child)'
+                        }
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        text: 'PDF',
+                        exportOptions: {
+                            columns: ':not(:last-child)'
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        text: 'Print',
+                        exportOptions: {
+                            columns: ':not(:last-child)'
+                        }
+                    }
+                ]
+            });
+        });
+    </script>
 </body>
 
 </html>
--- admin/project.php
+++ admin/project.php
@@ -3,10 +3,13 @@ session_start();
 error_reporting(0);
 include('../includes/db_connection.php');
 
+// Check if the admin is logged in
 if (empty($_SESSION['admin_login'])) {
     header('location: login.php');
-    exit(); // Add exit to stop further execution
+    exit();
 }
+
+// Page details
 $page_title = "Assigned Projects";
 $breadcrumb = "Project Management";
 ?>
@@ -24,19 +27,15 @@ $breadcrumb = "Project Management";
     <link rel="stylesheet" href="../assets/css/metisMenu.css">
     <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="../assets/css/slicknav.min.css">
-    <!-- amchart css -->
-    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-    <!-- Start datatable css -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
-    <!-- others css -->
     <link rel="stylesheet" href="../assets/css/typography.css">
     <link rel="stylesheet" href="../assets/css/default-css.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="stylesheet" href="../assets/css/responsive.css">
-    <!-- modernizr css -->
-    <script src="../assets/js/vendor/modernizr-2.8.3.min.js"></script>
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
     <style>
         .status-not-submitted {
             color: red;
@@ -46,14 +45,18 @@ $breadcrumb = "Project Management";
             color: green;
         }
     </style>
+    <script src="../assets/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 
 <body>
-    <!-- preloader area start -->
+    <!-- Preloader -->
     <div id="preloader">
         <div class="loader"></div>
     </div>
+    
+    <!-- Page Container -->
     <div class="page-container">
+        <!-- Sidebar Menu -->
         <div class="sidebar-menu">
             <div class="sidebar-header">
                 <div class="logo">
@@ -62,27 +65,26 @@ $breadcrumb = "Project Management";
             </div>
             <div class="main-menu">
                 <div class="menu-inner">
-                    <?php
-                    $page = 'project';
-                    include '../admin/layout/sidebar.php';
-                    ?>
+                    <?php include '../admin/layout/sidebar.php'; ?>
                 </div>
             </div>
         </div>
-
+        
+        <!-- Main Content Area -->
         <div class="main-content">
+            <!-- Header -->
             <?php include '../admin/layout/header.php'; ?>
-
+            
+            <!-- Page Content -->
             <div class="main-content-inner">
-                <!-- row area start -->
                 <div class="row">
-                    <!-- assigned projects area start -->
+                    <!-- Assigned Projects Area -->
                     <div class="col-12 mt-5">
                         <div class="card">
                             <div class="card-body">
                                 <div class="single-table">
                                     <div class="table-responsive">
-                                        <table class="table table-hover table-striped table-bordered progress-table text-center">
+                                        <table class="table table-hover table-striped table-bordered progress-table text-center" id="assignedProjectsTable">
                                             <thead class="text-uppercase">
                                                 <tr>
                                                     <th>S.N</th>
@@ -95,7 +97,7 @@ $breadcrumb = "Project Management";
                                                     <th>Status</th>
                                                     <th>Submission Date</th>
                                                     <th>Submitted Project File</th>
-                                                    <td></td>
+                                                    <th>Action</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
@@ -128,17 +130,14 @@ $breadcrumb = "Project Management";
                                                                 }
                                                                 ?>
                                                             </td>
-
-
                                                             <td class="<?php echo ($result->status == 0) ? 'status-not-submitted' : 'status-submitted'; ?>">
                                                                 <?php echo ($result->status == 0) ? 'Not Submitted' : 'Submitted'; ?>
                                                             </td>
                                                             <td>
                                                                 <?php echo $result->submission_date !== null ? htmlentities($result->submission_date) : 'Not submitted'; ?>
                                                             </td>
-
                                                             <td>
-                                                            <?php
+                                                                <?php
                                                                 if ($result->submitted_documents !== null) {
                                                                     echo '<a href="' . htmlentities($result->submitted_documents) . '" target="_blank">View Document</a>';
                                                                 } else {
@@ -160,16 +159,16 @@ $breadcrumb = "Project Management";
                             </div>
                         </div>
                     </div>
-                    <!-- assigned projects area end -->
                 </div>
-                <!-- row area end -->
             </div>
+
+            <!-- Footer -->
             <?php include '../admin/layout/footer.php'; ?>
         </div>
     </div>
-    <!-- jquery latest version -->
+    
+    <!-- jQuery and Bootstrap Scripts -->
     <script src="../assets/js/vendor/jquery-2.2.4.min.js"></script>
-    <!-- bootstrap 4 js -->
     <script src="../assets/js/popper.min.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
     <script src="../assets/js/owl.carousel.min.js"></script>
@@ -177,30 +176,67 @@ $breadcrumb = "Project Management";
     <script src="../assets/js/jquery.slimscroll.min.js"></script>
     <script src="../assets/js/jquery.slicknav.min.js"></script>
 
-    <!-- start chart js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <!-- start highcharts js -->
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <!-- start zingchart js -->
-    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
-    <script>
-        zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
-        ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
-    </script>
-    <!-- all line chart activation -->
-    <script src="../assets/js/line-chart.js"></script>
-    <!-- all pie chart -->
-    <script src="../assets/js/pie-chart.js"></script>
-
-    <!-- Start datatable js -->
+    <!-- DataTables JS and Buttons Extension -->
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
-
-    <!-- others plugins -->
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+    
+    <!-- Custom Scripts -->
     <script src="../assets/js/plugins.js"></script>
     <script src="../assets/js/scripts.js"></script>
+    
+    <!-- DataTable Initialization -->
+    <script>
+        $(document).ready(function() {
+            $('#assignedProjectsTable').DataTable({
+                dom: 'Bfrtip', // Add buttons UI
+                buttons: [
+                    {
+                        extend: 'copyHtml5',
+                        text: 'Copy',
+                        exportOptions: {
+                            columns: ':not(:last-child)' // Exclude action column
+                        }
+                    },
+                    {
+                        extend: 'excelHtml5',
+                        text: 'Excel',
+                        exportOptions: {
+                            columns: ':not(:last-child)'
+                        }
+                    },
+                    {
+                        extend: 'csvHtml5',
+                        text: 'CSV',
+                        exportOptions: {
+                            columns: ':not(:last-child)'
+                        }
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        text: 'PDF',
+                        exportOptions: {
+                            columns: ':not(:last-child)'
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        text: 'Print',
+                        exportOptions: {
+                            columns: ':not(:last-child)'
+                        }
+                    }
+                ]
+            });
+        });
+    </script>
 </body>
 
 </html>
